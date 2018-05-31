1440a0525b1a297dc92d8ed6361374fd1e60447e
TimG20 <tgoodman@ialign.com>
2018-05-31 14:05:35 -0700

    more setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'numeracy';
+  title = 'numeracy - a tool for reviewing financial statements ';
 }
